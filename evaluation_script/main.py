4a0a1722b9db043758e80b836fc14afc4a173c9f
Dan Griffith <dan.griffith@ll.mit.edu>
2020-02-24 10:37:07 -0500

    Initial goseek challenge commit.

--- evaluation_script/main.py
+++ evaluation_script/main.py
@@ -40,42 +40,39 @@ def evaluate(test_annotation_file, user_submission_file, phase_codename, **kwarg
         }
     """
     output = {}
-    if phase_codename == "dev":
-        print("Evaluating for Dev Phase")
-        output["result"] = [
-            {
-                "train_split": {
-                    "Metric1": random.randint(0, 99),
-                    "Metric2": random.randint(0, 99),
-                    "Metric3": random.randint(0, 99),
-                    "Total": random.randint(0, 99),
-                }
+    if phase_codename == "dev-groundtruth":
+        print("Evaluating for dev-groundtruth")
+
+    elif phase_codename == "dev-perception":
+        print("Evaluating for dev-perception")
+
+    elif phase_codename == "competition-groundtruth":
+        print("Evaluating for competition-groundtruth")
+
+    elif phase_codename == "competition-perception":
+        print("Evaluating for competition-perception")
+
+
+    # Roll the dice!
+    recall = random.random()
+    precision = random.random()
+    collisions = random.random()
+    actions = random.random()
+
+    print("Completed evaluation.")
+
+    output["result"] = [
+        {
+            "withheld_scenes": {
+                "Recall": recall,
+                "Precision": precision,
+                "Collisions": collisions,
+                "Actions": actions,
+                "Weighted Total": recall + 0.1*precision - 0.1*collisions - 0.1*actions
             }
-        ]
-        # To display the results in the result file
-        output["submission_result"] = output["result"][0]["train_split"]
-        print("Completed evaluation for Dev Phase")
-    elif phase_codename == "test":
-        print("Evaluating for Test Phase")
-        output["result"] = [
-            {
-                "train_split": {
-                    "Metric1": random.randint(0, 99),
-                    "Metric2": random.randint(0, 99),
-                    "Metric3": random.randint(0, 99),
-                    "Total": random.randint(0, 99),
-                }
-            },
-            {
-                "test_split": {
-                    "Metric1": random.randint(0, 99),
-                    "Metric2": random.randint(0, 99),
-                    "Metric3": random.randint(0, 99),
-                    "Total": random.randint(0, 99),
-                }
-            },
-        ]
-        # To display the results in the result file
-        output["submission_result"] = output["result"][0]
-        print("Completed evaluation for Test Phase")
+        }
+    ]
+
+    # To display the results in the result file
+    output["submission_result"] = output["result"][0]["withheld_scenes"]
     return output
